a7a126d685a9c1239f79bf7482a8bb5389396941
harisudhan8 <117929761+harisudhan8@users.noreply.github.com>
2024-07-25 16:04:26 +0530

    loop

---
+++ Day_1/loop.js
@@ -0,0 +1,8 @@
+console.log("Even and Odd numbers from 1 to 100:");
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
